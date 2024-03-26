0ada93cd2748755dc93ecb3971549cb166d1806c
Paco Leon <pacoleonpe@gmail.com>
2024-03-26 17:48:44 +0100

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string  = 'Hola mundo';
+  public counter: number = 10;
+
+increaseBy( value: number): void {
+  this.counter += value;
+}
+
+resetCounter(): void{
+  this.counter = 10;
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/components/counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroModule } from './heroes/hero.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
